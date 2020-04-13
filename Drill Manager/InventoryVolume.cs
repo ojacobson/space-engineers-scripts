5d10336a4beb3d046032efd1cdae6561c98159a8
Owen Jacobson <owen@grimoire.ca>
2020-04-12 20:27:14 -0400

    Drrill management script.

---
+++ Drill Manager/InventoryVolume.cs
@@ -0,0 +1,35 @@
+﻿using VRage;
+
+namespace IngameScript
+{
+    struct InventoryVolume
+    {
+        public static readonly InventoryVolume Zero = new InventoryVolume(0, 0);
+
+        public InventoryVolume(MyFixedPoint max, MyFixedPoint used)
+        {
+            Max = max;
+            Used = used;
+        }
+
+        public MyFixedPoint Max { get; }
+        public MyFixedPoint Used { get; }
+ 
+        public double Ratio
+        {
+            get
+            {
+                if (Max > 0)
+                    return (double)Used / (double)Max;
+
+                // Blocks with no inventory are always treated as "full".
+                return 1.0;
+            }
+        }
+
+        public static InventoryVolume operator +(InventoryVolume a, InventoryVolume b)
+        {
+            return new InventoryVolume(a.Max + b.Max, a.Used + b.Used);
+        }
+    }
+}
---
+++ Drill Manager/Program.cs
@@ -0,0 +1,120 @@
+﻿using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using VRage.Game.ModAPI.Ingame;
+using VRage.ObjectBuilders;
+
+namespace IngameScript
+{
+    partial class Program : MyGridProgram
+    {
+        public Program()
+        {
+            Runtime.UpdateFrequency = UpdateFrequency.Update10;
+        }
+
+        enum Goal
+        {
+            STOP,
+            CONTINUE,
+            START
+        }
+
+        void EnableAction(IMyFunctionalBlock drill)
+        {
+            drill.Enabled = true;
+        }
+
+        void DisableAction(IMyFunctionalBlock drill)
+        {
+            drill.Enabled = false;
+        }
+
+        void ContinueAction(IMyFunctionalBlock drill) {
+        }
+
+        public void Main(string argument, UpdateType updateSource)
+        {
+            var goal = InventoryGoal();
+
+            ApplyGoal(goal);
+        }
+
+        List<IMyFunctionalBlock> controlledBlocks = new List<IMyFunctionalBlock>();
+        void ApplyGoal(Goal goal)
+        {
+            GridTerminalSystem.GetBlocksOfType(controlledBlocks, IsControlled);
+            Action<IMyFunctionalBlock> goalAction = GoalAction(goal);
+            controlledBlocks.ForEach(goalAction);
+        }
+
+        Action<IMyFunctionalBlock> GoalAction(Goal goal)
+        {
+            Action<IMyFunctionalBlock> goalAction = ContinueAction;
+            switch (goal)
+            {
+                case Goal.START:
+                    goalAction = EnableAction;
+                    break;
+                case Goal.STOP:
+                    goalAction = DisableAction;
+                    break;
+            }
+
+            return goalAction;
+        }
+
+        bool IsControlled(IMyFunctionalBlock block) =>
+            IsLocal(block) && block.IsFunctional && IsControlTagged(block);
+
+        const string CONTROL_TAG = "[DM CONTROL]";
+        bool IsControlTagged(IMyTerminalBlock block) => block.CustomName.Contains(CONTROL_TAG);
+
+        List<IMyTerminalBlock> measuredBlocks = new List<IMyTerminalBlock>();
+        Goal InventoryGoal()
+        {
+            GridTerminalSystem.GetBlocksOfType(measuredBlocks, IsMonitoried);
+
+            var inventoryRatio = measuredBlocks
+                .Select(ContainerUsage)
+                .Aggregate(InventoryVolume.Zero, (a, b) => a + b)
+                .Ratio;
+
+            if (inventoryRatio < 0.1)
+                return Goal.START;
+            if (inventoryRatio < 0.9)
+                return Goal.CONTINUE;
+            return Goal.STOP;
+        }
+
+        InventoryVolume ContainerUsage(IMyTerminalBlock container)
+        {
+            InventoryVolume usage = InventoryVolume.Zero;
+
+            int inventories = container.InventoryCount;
+            for (var i = 0; i < inventories; ++i)
+            {
+                var inventory = container.GetInventory(i);
+                var inventoryUsage = InventoryUsage(inventory);
+
+                usage += inventoryUsage;
+            }
+
+            return usage;
+        }
+
+        InventoryVolume InventoryUsage(IMyInventory inventory) =>
+            new InventoryVolume(inventory.MaxVolume, inventory.CurrentVolume);
+
+        bool IsLocal(IMyTerminalBlock block) =>
+            Me.IsSameConstructAs(block);
+
+        bool IsMonitoried(IMyTerminalBlock block) =>
+            IsLocal(block) && block.IsFunctional && IsMonitorTagged(block);
+
+        const string MONITOR_TAG = "[DM MONITOR]";
+        bool IsMonitorTagged(IMyTerminalBlock block) => block.CustomName.Contains(MONITOR_TAG);
+    }
+}
